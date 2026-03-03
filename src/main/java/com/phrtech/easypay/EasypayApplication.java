58db3dcdcc313990c77aa7636fba53d7865e8d27
Pedro H. Rocha <phr.tech@outlook.com>
2026-03-03 11:19:28 -0300

    Initializing project

---
+++ src/main/java/com/phrtech/easypay/EasypayApplication.java
@@ -0,0 +1,13 @@
+package com.phrtech.easypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasypayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasypayApplication.class, args);
+	}
+
+}
